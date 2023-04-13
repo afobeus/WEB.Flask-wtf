c64ecbd4bc5276d8d8953a8fc3d861a6c0b1fece
Artem <artem.dzichkanets@mail.ru>
2023-04-13 21:55:21 +0300

    task 3

--- app.py
+++ app.py
@@ -14,5 +14,16 @@ def training(profession):
     return render_template("training.html", profession=profession)
 
 
+@app.route('/list_prof/<list_type>')
+def list_prof(list_type):
+    professions_list = ["инженер-исследователь", "пилот", "строитель", "экзобиолог",
+                        "врач", "инженер по терраформированию", "климатолог",
+                        "специалист по радиационной защите", "астрогеолог", "гляциолог",
+                        "инженер жизнеобеспечения", "метеоролог", "оператор марсохода",
+                        "киберинженер", "штурман", "пилот дронов"]
+
+    return render_template("professions.html", list_type=list_type)
+
+
 if __name__ == '__main__':
     app.run()
